1bed20d1ff6e4f5258a8d94df31996e068cd65a8
rslewenstein <rafasoaresjf@gmail.com>
2022-12-01 23:24:40 -0300

    removing spaces

--- invisible/invisible-client.lua
+++ invisible/invisible-client.lua
@@ -1,20 +1,18 @@
 RegisterNetEvent("invisible")
-	AddEventHandler("invisible", function()
-
-		local ped 	= PlayerPedId(-1)
-		local visible	= IsEntityVisible(ped)
-
-		if visible then
-			SetEntityVisible(ped, false)
-			notify("~g~Visibilidade desativada")
-		elseif not visible then
-			SetEntityVisible(ped, true)
-			notify("~b~Visibilidade ativada")
-		end
-	end)
-
-    function notify(msg)
-		SetNotificationTextEntry("STRING")
-		AddTextComponentString(msg)
-		DrawNotification(false, false)
+AddEventHandler("invisible", function()
+	local ped 	= PlayerPedId(-1)
+	local visible	= IsEntityVisible(ped)
+	if visible then
+		SetEntityVisible(ped, false)
+		notify("~g~Visibilidade desativada")
+	elseif not visible then
+		SetEntityVisible(ped, true)
+		notify("~b~Visibilidade ativada")
 	end
+end)
+
+function notify(msg)
+	SetNotificationTextEntry("STRING")
+	AddTextComponentString(msg)
+	DrawNotification(false, false)
+end
